f1a94155d0aa768eb7ca90d0e8fd47e9615cca93
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-01-29 13:36:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // Your code here.
+const itemsContainer = document.getElementById("itemsContainer");
+
+let isMouseDown = false;
+let startX, scrollLeft;
+
+itemsContainer.addEventListener("mousedown", (e) => {
+  isMouseDown = true;
+  startX = e.pageX - itemsContainer.offsetLeft;
+  scrollLeft = itemsContainer.scrollLeft;
+});
+
+
+itemsContainer.addEventListener("mouseleave", () => {
+  isMouseDown = false;
+});
+
+itemsContainer.addEventListener("mouseup", () => {
+  isMouseDown = false;
+});
+
+
+itemsContainer.addEventListener("mousemove", (e) => {
+  if (!isMouseDown) return;
+  e.preventDefault();
+  const x = e.pageX - itemsContainer.offsetLeft;
+  const walk = (x - startX) * 2; // You can adjust the multiplier to control the scrolling speed
+  itemsContainer.scrollLeft = scrollLeft - walk;
+});
